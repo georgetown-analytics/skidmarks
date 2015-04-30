4d0ba80d352b17c0b788fb082b650db480a2ad66
linwoodc3 <valinvescap@gmail.com>
2015-04-29 23:42:48 -0400

    Added stop and turn functions

--- bin/stop.py
+++ bin/stop.py
@@ -36,65 +36,76 @@ path = os.path.abspath(os.getcwd())
 # Main Functions
 ###############################################################################
 
-driver = raw_input('Pick a driver. Enter a number between 1-3612:\n')
-trip = raw_input('Pick a trip. Enter a number between 1-200:\n')
-path = os.path.abspath(os.getcwd())
-#pathtocsv = os.path.normpath(os.path.join(path,"output","test",str(driver),str(driver)+"_"+str(trip)+".csv"))
-pathtocsv = os.path.normpath(os.path.join(path,"output","trip",str(driver)+"_"+str(trip)+".csv"))
-
-print pathtocsv
-
-df = pd.read_csv(pathtocsv)
-
-df['start'] = 0
-df['end'] = 0
-
-print df[250:280][['Velocity (mph)','Change in Direction per s']]
-
-numbers = df.loc[1:][['Velocity (mph)', 'Change in Direction per s']] 
-
-
-val = pd.rolling_sum(numbers, window = 3)
-print len(val)
-print val[700:750]
-
-stops = val.loc[val['Increment Traveled (feet)'] <= 3].index
-
-if len(stops) < 1:
-	print "No measured stops"
-
-else: 
-
-	print "Seconds where high maneuvers are going on  \n", stops
-
-	#iterate through stops and flag start and end of stops in the dataframe
-
-	maneuvers = 0
-
-	df['start'][stops[0]] = 1	#the first index must be the start of the first turn
-
-	for i in range(1, len(stops)-1):
-		#print i
-		if stops[i] - 1 > stops[i-1]:
-			df['start'][stops[i]] = 1	#the current index and last index are more than 1 second apart so this must be the start of a new turn
-			df['end'][stops[i-1]] = 1	#the last index must also be the end of the last turn since we are beginning a new turn
-
-	df['end'][stops[len(stops)-1]] = 1	#the last index must be the end of the last turn
-
-
-	for index, row in df.iterrows():
-		if row['start'] == 1 or row['end'] == 1:
-			print index, row['Velocity (mph)']
-		if row['end'] == 1:
-			maneuvers += 1
-	print "Driver # %s made %d maneuver(s) in this trip" % (driver,maneuvers)
-
-	#return maneuvers
+def StopCount(driver,trip):
 
 
 
-'''
-Calculating stops and stops: Use the rolling window.  Divide "Change in Direction per s" by "Velocity (mph)" or vice versa to get a figure.
-These are windows of 5 seconds, so values that are closer to 1 indicate a driver that conducts turning-like maneuvers at high speeds.  We will 
-count the numbers of times a driver is in a high speed, high turn state. 
-'''
+	path = os.path.abspath(os.getcwd())
+	#pathtocsv = os.path.normpath(os.path.join(path,"output","test",str(driver),str(driver)+"_"+str(trip)+".csv"))
+	pathtocsv = os.path.normpath(os.path.join(path,"output","trip",str(driver)+"_"+str(trip)+".csv"))
+
+	print pathtocsv
+
+	df = pd.read_csv(pathtocsv)
+
+	df['start'] = 0
+	df['end'] = 0
+
+	print df[250:280][['Velocity (mph)','Change in Direction per s']]
+
+	numbers = df.loc[1:][['Velocity (mph)', 'Increment Traveled (feet)']] 
+
+
+	val = pd.rolling_sum(numbers, window = 3)
+	print len(val)
+	print val[700:750]
+
+	still = val.loc[val['Increment Traveled (feet)'] <= 3].index
+
+	if len(still) < 1:
+		print "No measured stops"
+
+	else: 
+
+		print "Seconds of consistent stops  \n", still
+
+		#iterate through still and flag start and end of still in the dataframe
+
+		stops = 0
+
+		df['start'][still[0]] = 1	#the first index must be the start of the first turn
+
+		for i in range(1, len(still)-1):
+			#print i
+			if still[i] - 1 > still[i-1]:
+				df['start'][still[i]] = 1	#the current index and last index are more than 1 second apart so this must be the start of a new turn
+				df['end'][still[i-1]] = 1	#the last index must also be the end of the last turn since we are beginning a new turn
+
+		df['end'][still[len(still)-1]] = 1	#the last index must be the end of the last turn
+
+
+		for index, row in df.iterrows():
+			if row['start'] == 1 or row['end'] == 1:
+				print index, row['Velocity (mph)']
+			if row['end'] == 1:
+				stops += 1
+		print "Driver # %s made %d stop(s) in this trip" % (driver,stops)
+
+		return stops
+
+
+
+	'''
+	Calculating still and still: Use the rolling window.  Divide "Change in Direction per s" by "Velocity (mph)" or vice versa to get a figure.
+	These are windows of 5 seconds, so values that are closer to 1 indicate a driver that conducts turning-like maneuvers at high speeds.  We will 
+	count the numbers of times a driver is in a high speed, high turn state. 
+	'''
+
+###############################################################################
+# 'Main' Function
+############################################################################### 
+
+if __name__ == '__main__':
+	driver = raw_input('Pick a driver. Enter a number between 1-3612:\n')
+	trip = raw_input('Pick a trip. Enter a number between 1-200:\n')
+	StopCount(driver,trip)
--- bin/turn.py
+++ bin/turn.py
@@ -36,65 +36,75 @@ path = os.path.abspath(os.getcwd())
 # Main Functions
 ###############################################################################
 
-driver = raw_input('Pick a driver. Enter a number between 1-3612:\n')
-trip = raw_input('Pick a trip. Enter a number between 1-200:\n')
-path = os.path.abspath(os.getcwd())
-#pathtocsv = os.path.normpath(os.path.join(path,"output","test",str(driver),str(driver)+"_"+str(trip)+".csv"))
-pathtocsv = os.path.normpath(os.path.join(path,"output","trip",str(driver)+"_"+str(trip)+".csv"))
-
-print pathtocsv
-
-df = pd.read_csv(pathtocsv)
-
-df['start'] = 0
-df['end'] = 0
-
-print df[250:280][['Velocity (mph)','Change in Direction per s']]
-
-numbers = df.loc[1:][['Velocity (mph)', 'Change in Direction per s']] 
+def TurnCount(driver,trip):
 
 
-val = pd.rolling_sum(numbers, window = 3)
-print len(val)
-print val[700:750]
+	path = os.path.abspath(os.getcwd())
+	#pathtocsv = os.path.normpath(os.path.join(path,"output","test",str(driver),str(driver)+"_"+str(trip)+".csv"))
+	pathtocsv = os.path.normpath(os.path.join(path,"output","trip",str(driver)+"_"+str(trip)+".csv"))
 
-turns = val.loc[val['Change in Direction per s'] >= 45].index
+	print pathtocsv
 
-if len(turns) < 1:
-	print "No measurable maneuvers"
+	df = pd.read_csv(pathtocsv)
 
-else: 
+	df['start'] = 0
+	df['end'] = 0
 
-	print "Seconds where high maneuvers are going on  \n", turns
+	print df[250:280][['Velocity (mph)','Change in Direction per s']]
 
-	#iterate through turns and flag start and end of turns in the dataframe
-
-	maneuvers = 0
-
-	df['start'][turns[0]] = 1	#the first index must be the start of the first turn
-
-	for i in range(1, len(turns)-1):
-		#print i
-		if turns[i] - 1 > turns[i-1]:
-			df['start'][turns[i]] = 1	#the current index and last index are more than 1 second apart so this must be the start of a new turn
-			df['end'][turns[i-1]] = 1	#the last index must also be the end of the last turn since we are beginning a new turn
-
-	df['end'][turns[len(turns)-1]] = 1	#the last index must be the end of the last turn
+	numbers = df.loc[1:][['Velocity (mph)', 'Change in Direction per s']] 
 
 
-	for index, row in df.iterrows():
-		if row['start'] == 1 or row['end'] == 1:
-			print index, row['Velocity (mph)']
-		if row['end'] == 1:
-			maneuvers += 1
-	print "Driver # %s made %d maneuver(s) in this trip" % (driver,maneuvers)
+	val = pd.rolling_sum(numbers, window = 3)
+	print len(val)
+	print val[700:750]
 
-	#return maneuvers
+	turns = val.loc[val['Change in Direction per s'] >= 45].index
+
+	if len(turns) < 1:
+		print "No measurable maneuvers"
+
+	else: 
+
+		print "Seconds where high maneuvers are going on  \n", turns
+
+		#iterate through turns and flag start and end of turns in the dataframe
+
+		maneuvers = 0
+
+		df['start'][turns[0]] = 1	#the first index must be the start of the first turn
+
+		for i in range(1, len(turns)-1):
+			#print i
+			if turns[i] - 1 > turns[i-1]:
+				df['start'][turns[i]] = 1	#the current index and last index are more than 1 second apart so this must be the start of a new turn
+				df['end'][turns[i-1]] = 1	#the last index must also be the end of the last turn since we are beginning a new turn
+
+		df['end'][turns[len(turns)-1]] = 1	#the last index must be the end of the last turn
+
+
+		for index, row in df.iterrows():
+			if row['start'] == 1 or row['end'] == 1:
+				print index, row['Velocity (mph)']
+			if row['end'] == 1:
+				maneuvers += 1
+		print "Driver # %s made %d maneuver(s) in this trip" % (driver,maneuvers)
+
+		return maneuvers
 
 
 
-'''
-Calculating turns and stops: Use the rolling window.  Divide "Change in Direction per s" by "Velocity (mph)" or vice versa to get a figure.
-These are windows of 5 seconds, so values that are closer to 1 indicate a driver that conducts turning-like maneuvers at high speeds.  We will 
-count the numbers of times a driver is in a high speed, high turn state. 
-'''
+	'''
+	Calculating turns and stops: Use the rolling window.  Divide "Change in Direction per s" by "Velocity (mph)" or vice versa to get a figure.
+	These are windows of 5 seconds, so values that are closer to 1 indicate a driver that conducts turning-like maneuvers at high speeds.  We will 
+	count the numbers of times a driver is in a high speed, high turn state. 
+	'''
+
+###############################################################################
+# 'Main' Function
+############################################################################### 
+
+if __name__ == '__main__':
+	driver = raw_input('Pick a driver. Enter a number between 1-3612:\n')
+	trip = raw_input('Pick a trip. Enter a number between 1-200:\n')
+	TurnCount(driver,trip)
