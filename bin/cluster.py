c6a730ad67556668a31a8b3c858ec05e3067a079
linwoodc3 <valinvescap@gmail.com>
2015-05-07 02:18:10 -0400

    Running more tests

--- bin/cluster.py
+++ bin/cluster.py
@@ -14,6 +14,7 @@ from sklearn.preprocessing import Imputer
 from sklearn import linear_model
 import statsmodels.api as sm
 from statsmodels.sandbox.regression.predstd import wls_prediction_std
+from sklearn import preprocessing
 
 # Some colors for later
 colors = np.array([x for x in 'bgrcmykbgrcmykbgrcmykbgrcmyk'])
@@ -47,15 +48,23 @@ with open("./lin.csv") as in_data:
 
 #load all numeric data into an array. The offense column from the crime data
 #is excluded
-as_array = np.asfarray(crime_data[['Average Velocity (mph)','Displacement']])#'Max Velocity', 'Velocity Stdev','Average Acceleration (mph per s)', 'Max Acceleration (mph per s)', ' Acceleration Stdev','Displacement','Total Distance Traveled','Max Direction Change per sec', ' Direction Stdev','Time (s)', 'Turns', 'Aggressive Turns', 'Stops', 'Large Deceleration Events', 'Deceleration Events', 'Max Deceleration Event']])
+as_array = np.asfarray(crime_data[['Velocity Stdev','Aggressive Turns']])#'Max Velocity', 'Velocity Stdev','Average Acceleration (mph per s)', 'Max Acceleration (mph per s)', ' Acceleration Stdev','Displacement','Total Distance Traveled','Max Direction Change per sec', ' Direction Stdev','Time (s)', 'Turns', 'Aggressive Turns', 'Stops', 'Large Deceleration Events', 'Deceleration Events', 'Max Deceleration Event']])
 
 #number of groups
-n_clusters=5
+n_clusters=4
 
+#Correct missing data 
 imputer = Imputer(missing_values="NaN", strategy="mean")
 patched = imputer.fit_transform(as_array)
 
-print patched
+# Preprocessing tricks
+#patched = StandardScaler().fit_transform(patched)
+#patched = scale(patched, axis=0, with_mean=True)
+
+patched = preprocessing.normalize(patched, norm='l2')
+
+#min_max_scaler = preprocessing.MinMaxScaler()
+#patched = min_max_scaler.fit_transform(patched)
 
 
 
@@ -71,12 +80,16 @@ cluster.fit(patched)
 labels = cluster.labels_
 
 
-#pdict = create_ordered_dict(crime_data, "labels")
-'''
-  # Fit the model with our algorithm
-model = MiniBatchKMeans(n_clusters=3)
-model.fit(as)
-'''
+#copy dataframe (may be memory intensive but just for illustration)
+skid_data = crime_data.copy()
+#print pd.Series(classified_data)
+#print pd.Series(prediction_data)
+skid_data['Cluster Class'] = pd.Series(labels, index=skid_data.index)
+print skid_data.describe()
+print skid_data
+#print list(skid_data.columns)
+skid_data.plot( x = 'Aggressive Turns', y = 'Cluster Class', kind = 'scatter')
+plt.show()
 # Make Predictions
 predictions = cluster.predict(patched)
 
